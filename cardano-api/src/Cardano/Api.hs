9de61a59fa44f986853691c524fee7d0a2f60243
John Ky <john.ky@iohk.io>
2023-08-12 11:32:55 +1000

    New justFeatureInEra and justInEraFeature functions

--- cardano-api/src/Cardano/Api.hs
+++ cardano-api/src/Cardano/Api.hs
@@ -30,6 +30,8 @@ module Cardano.Api (
     -- * Feature support
     FeatureInEra(..),
     inEraFeature,
+    justFeatureInEra,
+    justInEraFeature,
     maybeFeatureInEra,
     featureInShelleyBasedEra,
     inShelleyBasedEraFeature,
@@ -996,6 +998,7 @@ import           Cardano.Api.DeserialiseAnyOf
 import           Cardano.Api.DRepMetadata
 import           Cardano.Api.EraCast
 import           Cardano.Api.Eras
+import           Cardano.Api.Eras.Core
 import           Cardano.Api.Error
 import           Cardano.Api.Feature
 import           Cardano.Api.Feature.AlonzoEraOnly
